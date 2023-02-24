90df8e01d31c5633aa6a1701e808593023d38408
Andrew Brtsvch <09.parabellum.86@gmail.com>
2023-02-24 13:59:10 +0300

    1.1 Добавлена статика, улучшен внешний вид

--- feedback/forms.py
+++ feedback/forms.py
@@ -12,15 +12,36 @@ class FeedbackForm(forms.Form):
             'required': 'Поле не может быть пустым',
         }
     )
-    # surname = forms.CharField()
-    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'columns': 5}))
-    rating = forms.IntegerField(
-        label="Рейтинг",
-        max_value=100,
-        min_value=0,
+    surname = forms.CharField(
+        label="Фамилия",
+        max_length=12,
+        min_length=3,
         error_messages={
-            'max_value': 'Рейтинг должен быть в пределах от 0 до 100',
-            'min_value': 'Рейтинг должен быть в пределах от 0 до 100',
+            'max_length': 'Символов должно быть меньше',
+            'min_length': 'Символов должно быть больше',
             'required': 'Поле не может быть пустым',
         }
     )
+    feedback = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 4, 'columns': 5}),
+        label="Отзыв",
+        max_length=120,
+        min_length=3,
+        error_messages={
+            'max_length': 'Символов должно быть меньше',
+            'min_length': 'Символов должно быть больше',
+            'required': 'Поле не может быть пустым',
+        }
+    )
+
+
+    # rating = forms.IntegerField(
+    #     label="Рейтинг",
+    #     max_value=100,
+    #     min_value=0,
+    #     error_messages={
+    #         'max_value': 'Рейтинг должен быть в пределах от 0 до 100',
+    #         'min_value': 'Рейтинг должен быть в пределах от 0 до 100',
+    #         'required': 'Поле не может быть пустым',
+    #     }
+    # )
