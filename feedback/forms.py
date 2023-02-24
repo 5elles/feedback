b76059b6257ad7e391c03753d9dcd6b81f23362e
Andrew Brtsvch <09.parabellum.86@gmail.com>
2023-02-24 12:15:46 +0300

    first commit

---
+++ feedback/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField(
+        label="Имя",
+        max_length=12,
+        min_length=3,
+        error_messages={
+            'max_length': 'Символов должно быть меньше',
+            'min_length': 'Символов должно быть больше',
+            'required': 'Поле не может быть пустым',
+        }
+    )
+    # surname = forms.CharField()
+    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'columns': 5}))
+    rating = forms.IntegerField(
+        label="Рейтинг",
+        max_value=100,
+        min_value=0,
+        error_messages={
+            'max_value': 'Рейтинг должен быть в пределах от 0 до 100',
+            'min_value': 'Рейтинг должен быть в пределах от 0 до 100',
+            'required': 'Поле не может быть пустым',
+        }
+    )
